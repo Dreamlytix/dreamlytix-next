52401a459a1b978272de0e6ff5a8b3ced9085970
chiranjiv95 <chiranjiv.mohanty10@gmail.com>
2025-11-23 09:04:12 +0530

    feat(ui): add initial header and hero sections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <div className="flex-1">
-          {children}
-        </div>
+        <Header />
+        <div className="flex-1">{children}</div>
         <Footer />
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 import Contact from "@/components/sections/Contact/Contact";
+import Hero from "@/components/sections/Hero/Hero";
 
 export default function Home() {
   return (
-    <> <div className="flex min-h-screen items-center justify-center font-sans bg-white dark:bg-black">
-      <main className="flex flex-col items-center justify-center text-center gap-4">
-        <h1 className="text-5xl font-semibold tracking-tight text-black dark:text-zinc-50">
-          Dreamlytix
-        </h1>
-        <p className="text-lg text-gray-600 dark:text-zinc-400">
-          You Dream, We Build.
-        </p>
-      </main>
-    </div>
+    <>
+      <Hero />
       <Contact />
     </>
   );
