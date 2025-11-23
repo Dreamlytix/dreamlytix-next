026a5c37660020b827155b5eba0b7666eb0efa6c
chiranjiv95 <chiranjiv.mohanty10@gmail.com>
2025-11-23 12:04:25 +0530

    feat: add TechStack section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Contact from "@/components/sections/Contact/Contact";
 import Hero from "@/components/sections/Hero/Hero";
 import Services from "@/components/sections/Services/Services";
+import TechStack from "@/components/sections/TechStack/TechStack";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Services />
+      <TechStack />
       <Contact />
     </>
   );
