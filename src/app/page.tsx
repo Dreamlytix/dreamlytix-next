31e0bebd7005d5ab2c2d5f2e186a4dc7515f0682
chiranjiv95 <chiranjiv.mohanty10@gmail.com>
2025-12-03 23:56:42 +0530

    update action button onClick

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,9 @@ export default function Home() {
       <Portfolio />
       <Testimonials />
       <Team />
-      <Contact />
+      <div id="contact">
+        <Contact />
+      </div>
     </>
   );
 }
